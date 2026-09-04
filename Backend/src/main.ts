f585a0d092245ebe1175d62ba2f5c967b423ba50
roshanrhn11 <roshanrhn11@gmail.com>
2026-09-04 10:52:56 +0530

    Fix production CORS

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -8,6 +8,11 @@ async function bootstrap() {
   const allowedOrigins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+
+    // Production frontend
+    "https://portfolio-frontend-9e6sb6kwu-rainy-stack.vercel.app",
+
+    // Environment variable
     process.env.FRONTEND_URL,
   ].filter(Boolean);
 
